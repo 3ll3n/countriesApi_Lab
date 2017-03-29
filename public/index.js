696017963066783a0a0def7636e5f7c11d36a934
3ll3n <ellengrafton01@gmail.com>
2017-03-29 14:30:45 +0100

    Created functions to display country name, capital city and population

--- public/index.js
+++ public/index.js
@@ -20,7 +20,20 @@ var requestComplete = function() {
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
   
-  populateList(countries);
+  // populateList(countries);
+
+  populateSelector(countries); 
+};
+
+var countryRequestComplete = function(){
+  if(this.status !== 200) {
+    return;
+  }
+  // grab the response text
+  var jsonString = this.responseText;
+  var country = JSON.parse(jsonString);
+
+  populateCountryResult(country[0]);
 };
 
 var populateList = function(countries) {
@@ -33,13 +46,36 @@ var populateList = function(countries) {
   });
 };
 
+var populateSelector = function(countries) {
+  var dropDown = document.getElementById("select-country")
+
+  countries.forEach(function(country) {
+    var option = document.createElement("option");
+    option.innerText = country.name; 
+    dropDown.appendChild(option);
+  });
+};
+
+var populateCountryResult = function(country) {
+  console.log(country);
+      var pTag = document.querySelector("#country-result");
+      pTag.innerText = "Country name: " + country.name + "\n Capital city: " + country.capital + "\n Population: " + country.population;
+};
+
+var handleSelectChanged = function() {
+  var selectedCountry = this.value;
+  var countryUrl = "https://restcountries.eu/rest/v2/name/" + selectedCountry;
+  console.log(selectedCountry);
+
+  makeRequest(countryUrl, countryRequestComplete);
+};
+
 var app = function(){
   var url = "https://restcountries.eu/rest/v2";
+    makeRequest(url, requestComplete);
 
-  var button = document.querySelector("button");
-  button.onclick = function() {
-  makeRequest(url, requestComplete);
-  };
-}
+    var select = document.querySelector("select");
+    select.onchange = handleSelectChanged;
+};
 
 window.onload = app;
